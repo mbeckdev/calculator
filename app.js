706f7cbd18e81d048ec35ebfbb396e1f0aac3b4f
mbeckdev <mbeckdev42@gmail.com>
2021-02-25 18:05:59 -0700

    Add event listeners to numbers

--- app.js
+++ app.js
@@ -1,38 +1,66 @@
 'use strict';
 
-      function add(a, b) {
-        return a + b;
-      }
-      function subtract(a, b) {
-        return a - b;
-      }
-      function multiply(a, b) {
-        return a * b;
-      }
-      function divide(a, b) {
-        return a / b;
-      }
-      function operate(num1, operator, num2) {
-        switch (operator) {
-          case '+':
-            return add(num1, num2);
-          // break;
-          case '-':
-            return subtract(num1, num2);
-          // break;
-          case '*':
-            return multiply(num1, num2);
-          // break;
-          case '/':
-            return divide(num1, num2);
-          // break;
-        }
-      }
-      console.log(add(1, 2.1));
-      console.log(operate(1, '+', 2.1));
-      console.log(subtract(2.1, 1));
-      console.log(operate(2.1, '-', 1));
-      console.log(multiply(2.1, 2));
-      console.log(operate(2.1, '*', 2));
-      console.log(divide(9.3, 3));
-      console.log(operate(9.3, '/', 3));
+function add(a, b) {
+return a + b;
+}
+function subtract(a, b) {
+return a - b;
+}
+function multiply(a, b) {
+return a * b;
+}
+function divide(a, b) {
+return a / b;
+}
+function operate(num1, operator, num2) {
+switch (operator) {
+    case '+':
+    return add(num1, num2);
+    // break;
+    case '-':
+    return subtract(num1, num2);
+    // break;
+    case '*':
+    return multiply(num1, num2);
+    // break;
+    case '/':
+    return divide(num1, num2);
+    // break;
+}
+}
+console.log(add(1, 2.1));
+console.log(operate(1, '+', 2.1));
+console.log(subtract(2.1, 1));
+console.log(operate(2.1, '-', 1));
+console.log(multiply(2.1, 2));
+console.log(operate(2.1, '*', 2));
+console.log(divide(9.3, 3));
+console.log(operate(9.3, '/', 3));
+
+// Functions that populate teh display when you clik the number buttons
+// loop through all that have a class of 'number' and add event listener
+
+function addNumberEventListeners() {
+    let numbers = Array.from(document.querySelectorAll('.numbers'));
+    numbers.forEach((e) => {
+        // console.log(e);
+        e.addEventListener('click', populateDisplay);
+
+    });
+    // console.log(numbers);
+}
+addNumberEventListeners();
+
+const display = document.getElementById('screen');
+let displayNumber = 0;
+function populateDisplay(e) {
+    console.log(e);
+    console.log(e.target);
+    // console.log(e.target.getAttribute('id'));
+    console.log(e.target.textContent);
+
+    displayNumber = displayNumber + e.target.textContent;
+    
+    console.log(displayNumber); //is a string
+    display.textContent = displayNumber;
+}
